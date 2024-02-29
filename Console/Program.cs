7679a6006aaec59a8d635043f2843859165551a0
Daniel Cujba <daniel.cujba.54@gmail.com>
2024-02-29 22:23:09 +0200

    Initial Commit

---
+++ Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApp;
+
+class Program
+{
+    public static Drug drug = new Drug{
+        Name = "Paracetamol",
+        Price = "10"
+    };
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(drug.Name);
+    }
+}
